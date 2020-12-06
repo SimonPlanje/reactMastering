92b50bf6db43b13ceb386fe4bbce57762361b12a
Simon Planje <simon.planje2000@gmail.com>
2020-12-06 13:38:04 +0100

    added react app and first components

---
+++ src/components/Song.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Song() {
+
+  return (
+    <div className="song-container">
+      <h1>Picture</h1>
+      <h1>song name</h1>
+      <h1>Artist</h1>
+    </div>
+  );
+}
+
+export default Song;
